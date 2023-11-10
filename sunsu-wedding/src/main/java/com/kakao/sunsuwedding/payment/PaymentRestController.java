29f1b45c5ebbe1fee552e669559b333472288500
Youngchae Cheon <82564901+chaee813@users.noreply.github.com>
2023-11-11 00:48:55 +0900

    Update PaymentRestController.java

--- sunsu-wedding/src/main/java/com/kakao/sunsuwedding/payment/PaymentRestController.java
+++ sunsu-wedding/src/main/java/com/kakao/sunsuwedding/payment/PaymentRestController.java
@@ -4,6 +4,7 @@ import com.kakao.sunsuwedding._core.security.CustomUserDetails;
 import com.kakao.sunsuwedding._core.utils.ApiUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/payment")
@@ -22,6 +24,7 @@ public class PaymentRestController {
     @PostMapping("/save")
     public ResponseEntity<?> save(@AuthenticationPrincipal CustomUserDetails userDetails,
                                   @Valid @RequestBody PaymentRequest.SaveDTO requestDTO) {
+        log.debug("\nController save EXECUTED 1\n");
         paymentServiceImpl.save(userDetails.getUser().getId(), requestDTO);
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
@@ -30,6 +33,7 @@ public class PaymentRestController {
     @PostMapping("/approve")
     public ResponseEntity<?> approve(@AuthenticationPrincipal CustomUserDetails userDetails,
                                      @Valid @RequestBody PaymentRequest.ApproveDTO requestDTO) {
+        log.debug("\nController approve EXECUTED 1\n");
         paymentServiceImpl.approve(userDetails.getUser().getId(), requestDTO);
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
